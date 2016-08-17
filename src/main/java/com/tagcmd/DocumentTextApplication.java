cab6ff95fd199ea62b8ef88ab5e2572d5bd65ce8
matthew.tomich <matthew.tomich@gmail.com>
2016-08-17 22:39:41 +0100

    first commit

---
+++ src/main/java/com/tagcmd/DocumentTextApplication.java
@@ -0,0 +1,29 @@
+package com.tagcmd;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class DocumentTextApplication extends Application<DocumentTextConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new DocumentTextApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "DocumentText";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<DocumentTextConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final DocumentTextConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
---
+++ src/main/java/com/tagcmd/DocumentTextConfiguration.java
@@ -0,0 +1,10 @@
+package com.tagcmd;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class DocumentTextConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
